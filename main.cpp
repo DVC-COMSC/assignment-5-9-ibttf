699073836dde543833b32cfb45103b98caa276bb
roylee0912 <leeroy0912@gmail.com>
2024-03-08 16:41:48 -0800

    done i think

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@ using namespace std;
 int main()
 {
 	int prev, current;
+	while (cin >> current)
+	{
+		if (current > prev)
+		{
+			cout  << endl;
+			return 0;
+		}
+		else
+		{
+			cout << current << endl;
+		}
+		prev = current;
+	}
 	/* ****************************
 	// Complete this code.
 	*************************** */
